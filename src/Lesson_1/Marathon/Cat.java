40fb893bf88b20a72f8154764216b1029e2e3da7
doomind <doomind@gmail.com>
2019-04-28 16:07:08 +0300

    Lesson_1

---
+++ src/Lesson_1/Marathon/Cat.java
@@ -0,0 +1,7 @@
+package Lesson_1.Marathon;
+
+public class Cat extends Animal implements Competitor{
+    public Cat(String name) {
+        super("Кот", name, 200, 20, 0);
+    }
+}
---
+++ src/Lesson_1/Marathon/Course.java
@@ -0,0 +1,24 @@
+package Lesson_1.Marathon;
+
+public class Course {
+    private Obstacle[] obstacles;
+    private Team[] teams;
+
+    Course (Team team1, Team team2, Obstacle... obstacles) {
+        this.teams = new Team[]{team1, team2};
+        this.obstacles = obstacles;
+    }
+
+    public void doIt () {
+        for (Obstacle obstacle:
+             this.obstacles) {
+            for (Team team:
+                 teams) {
+                for (Competitor competitor:
+                     team.competitors) {
+                        obstacle.doIt(competitor);
+                }
+            }
+        }
+    }
+}

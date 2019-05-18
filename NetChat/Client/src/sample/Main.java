d3f26e38b3b8e846c7e99ede36436e614eaa34e5
doomind <doomind@gmail.com>
2019-05-18 18:56:53 +0300

    NetChat_Lesson4

---
+++ NetChat/Client/src/sample/Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Chat client");
+        ObservableList icons = primaryStage.getIcons();
+        icons.add(new Image("./resources/16x16_icon.png"));
+        icons.add(new Image("./resources/32x32_icon.png"));
+        icons.add(new Image("./resources/64x64_icon.png"));
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
